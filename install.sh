2c694c5528df005fba117fdcaa3b0d1e1d96e7c8
kalayciali <kalayciali857@gmail.com>
2022-03-13 17:45:24 +0300

    install

---
+++ install.sh
@@ -0,0 +1,4 @@
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+ln -s ~/git/dotfiles/vimconf ~/.vimrc
+ln -s ~/git/dotfiles/zshconf ~/.zshrc
